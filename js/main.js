268d94c7fb82fe8fcd56ee6fba260eaec6913408
walter Rodriguez <walter.rodriguez1992@gmail.com>
2022-09-28 21:01:29 -0500

    agregando funcionalidad al boton de menu en mobile

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
 
 const menuEmail = document.querySelector(".navbar-email")
-const desktopMenu = document.querySelector(".desktop-menu")
 const arrowEmail =document.querySelector(".email-arrow")
+const desktopMenu = document.querySelector(".desktop-menu")
+
+
+const menuIcono = document.querySelector(".menu")
+const mobileMenu = document.querySelector(".mobile-menu")
 
 menuEmail.addEventListener("click", showDesktopMenu )
 arrowEmail.addEventListener("click", showDesktopMenu )
+menuIcono.addEventListener("click", showMenuMobile)
 
 function showDesktopMenu(){
 
     desktopMenu.classList.toggle("inactive")
+}
+function showMenuMobile(){
+    mobileMenu.classList.toggle("inactive")
 
 }
