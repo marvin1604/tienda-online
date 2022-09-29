554df7dafa20d66ac0c7b03839b6c4450256246a
walter Rodriguez <walter.rodriguez1992@gmail.com>
2022-09-28 21:33:59 -0500

    avance de funcionalidad a carrito de compras

--- js/main.js
+++ js/main.js
@@ -7,9 +7,14 @@ const desktopMenu = document.querySelector(".desktop-menu")
 const menuIcono = document.querySelector(".menu")
 const mobileMenu = document.querySelector(".mobile-menu")
 
+const carImg = document.querySelector(".car-img")
+const myOrder = document.querySelector(".my-order")
+
 menuEmail.addEventListener("click", showDesktopMenu )
 arrowEmail.addEventListener("click", showDesktopMenu )
 menuIcono.addEventListener("click", showMenuMobile)
+carImg.addEventListener("click", showOrderCar)
+
 
 function showDesktopMenu(){
 
@@ -17,5 +22,7 @@ function showDesktopMenu(){
 }
 function showMenuMobile(){
     mobileMenu.classList.toggle("inactive")
-
+}
+function showOrderCar(){
+    myOrder.classList.toggle("inactive")
 }
